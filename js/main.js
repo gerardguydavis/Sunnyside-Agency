1d9706b8b477ccf8b5d594422d800f26c665363f
Gerard Davis <gerardguydavis@outlook.com>
2021-08-02 17:14:56 -0400

    Add mobile drop menu

---
+++ js/main.js
@@ -0,0 +1,13 @@
+const navMenu = document.getElementById("navmenu");
+const hamburger = document.getElementById("hamburger");
+
+hamburger.addEventListener("click", function () {
+    console.log(navMenu.classList)
+    if (navMenu.classList.contains("hide")) {
+        navMenu.classList.add("dropmenu");
+        navMenu.classList.remove("hide");
+    } else if (navMenu.classList.contains("dropmenu")) {
+        navMenu.classList.add("hide");
+        navMenu.classList.remove("dropmenu");
+    }
+});
