6c14199eaa301526751aabd75af1074e42f14da5
Gerard Davis <gerardguydavis@outlook.com>
2021-08-02 17:52:32 -0400

    Add slide down animation to drop menu

--- js/main.js
+++ js/main.js
@@ -2,6 +2,7 @@ const navMenu = document.getElementById("navmenu");
 const hamburger = document.getElementById("hamburger");
 
 hamburger.addEventListener("click", function () {
+    console.log(navMenu.classList)
     if (navMenu.classList.contains("hide")) {
         navMenu.classList.add("dropmenu");
         navMenu.classList.remove("hide");
